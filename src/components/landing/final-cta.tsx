242be7620bf0107a0d0442de7cc06c6ff84a5dd4
Rachando o Bico <rachandobico23@gmail.com>
2025-12-10 19:03:20 +0000

    Reduza mais um pouquinho

--- src/components/landing/final-cta.tsx
+++ src/components/landing/final-cta.tsx
@@ -4,7 +4,7 @@ export function FinalCTA() {
   return (
     <section className="w-full bg-card py-12 md:py-24 lg:py-32">
       <div className="container mx-auto flex flex-col items-center gap-6 px-4 text-center md:px-6">
-        <h2 className="text-3xl tracking-tighter sm:text-4xl">
+        <h2 className="text-2xl tracking-tighter sm:text-3xl">
           A introdução alimentar
           <br />
           do seu bebê com segurança
