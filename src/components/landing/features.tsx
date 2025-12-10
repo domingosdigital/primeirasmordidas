22355b76a29e14f1101122541e673e1bebd5b3fe
Rachando o Bico <rachandobico23@gmail.com>
2025-12-10 16:37:14 +0000

    Quebre a linha no titulo "O que você encontra no App"
    Deixe:
    "O que voc

--- src/components/landing/features.tsx
+++ src/components/landing/features.tsx
@@ -62,7 +62,8 @@ export function Features() {
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <div className="space-y-2">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-              O que você encontra no App
+              O que você <br />
+              encontra no App
             </h2>
             <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
               Recursos pensados para tornar a introdução alimentar uma jornada
