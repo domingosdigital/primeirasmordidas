05e52eb2be1fbc1524e9ab9edda79608ad4d761e
Rachando o Bico <rachandobico23@gmail.com>
2025-12-10 18:55:59 +0000

    Deixe o titulo
    "Por que o Primeiras
    Mordidas é diferente?"
    
    nessa form

--- src/components/landing/value-prop.tsx
+++ src/components/landing/value-prop.tsx
@@ -20,8 +20,9 @@ export function ValueProp() {
       <div className="container mx-auto px-4 md:px-6">
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
-            Por que o Primeiras Mordidas
-            <br />é diferente?
+            Por que o Primeiras
+            <br />
+            Mordidas é diferente?
           </h2>
         </div>
         <div className="mx-auto mt-12 grid max-w-5xl gap-8 md:grid-cols-2 md:gap-12">
