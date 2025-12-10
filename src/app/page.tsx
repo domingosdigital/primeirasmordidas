c430dc3b31395f2d313f1f3774e0a7d699a75053
Rachando o Bico <rachandobico23@gmail.com>
2025-12-10 17:14:39 +0000

    BOTE A SESSAO DE DEPOIMENTOS ACIMA DO DA SESSAO "O que você encontra no

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
       <main className="flex-1">
         <Hero />
         <TargetAudience />
-        <Features />
         <Testimonials />
+        <Features />
         <ValueProp />
         <Offer />
         <Guarantee />
