2a85243f6bb28972df481b889036bf5fcb930e3a
Rachando o Bico <rachandobico23@gmail.com>
2025-12-11 22:11:26 +0000

    Deixe "Único Aplicativo" com as primeiras letras maiúsculas.

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -29,7 +29,7 @@ export function Hero() {
                 </span>{" "}
                 -{" "}
                 <span className="font-bold text-foreground">
-                  tudo reunido em um único aplicativo para facilitar a correria
+                  tudo reunido em um Único Aplicativo para facilitar a correria
                   do dia a dia.
                 </span>
               </p>
