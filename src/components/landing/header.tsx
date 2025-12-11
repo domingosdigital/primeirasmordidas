a962ae5fb8b8503ced2f2e5c0f5f12d1627aa307
Rachando o Bico <rachandobico23@gmail.com>
2025-12-11 22:03:51 +0000

    Adicione uma leve osmbra na logo.

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -2,7 +2,7 @@ export function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 flex h-16 items-center justify-center border-b bg-primary">
       <div className="container flex items-center justify-center">
-        <h1 className="text-3xl font-bold tracking-tighter text-primary-foreground">
+        <h1 className="text-3xl font-bold tracking-tighter text-primary-foreground [text-shadow:0_1px_2px_rgba(0,0,0,0.2)]">
           BabyFood®
         </h1>
       </div>
