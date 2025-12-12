0750ee147ab11de9e31f82ecf876c8cbcc4bc297
Rachando o Bico <rachandobico23@gmail.com>
2025-12-12 19:37:32 +0000

    Quebre a linha:
    "Debora Valentin
    Nutricionista, mãe de 2"
    
    Deixe centra

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -90,7 +90,7 @@ export function Hero() {
                     complicações."
                   </p>
                   <p className="mt-2 text-sm text-muted-foreground">
-                    Debora Valentin - Nutricionista, mãe de 2
+                    Debora Valentin <br /> Nutricionista, mãe de 2
                   </p>
                 </div>
               </div>
